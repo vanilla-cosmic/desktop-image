e6f8ce1f8f37810d3b9ff593d68bf6d3d89a7117
mirkobrombin <brombin@mirko.pm>
2025-01-27 23:29:46 +0100

    feat: use in-house built packages for cosmic

---
+++ includes.container/usr/share/get-cosmic-deb-urls.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+API_URL="https://api.github.com/repos/vanilla-cosmic/cosmic-epoch/releases/tags/continuous"
+
+curl -s "$API_URL" | jq -r '.assets[]
+  | select(.name | endswith(".deb"))
+  | select(.name | contains("dbgsym") | not)
+  | .browser_download_url'
--- includes.container/usr/share/get-cosmic.sh
+++ includes.container/usr/share/get-cosmic.sh
@@ -1,42 +1,13 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-# -----------------------------------------------------------------------------
-# This script is used to download and install the COSMIC Desktop Components
-# from the COPR repository. Since the components are not available as DEB
-# packages, we transform the RPM packages to DEB packages using the alien
-# tool and install them using dpkg.
-#
-# Source: https://copr.fedorainfracloud.org/coprs/ryanabx/cosmic-epoch/
-#
-# Notes for maintainers:
-# - Keep the script up-to-date with the latest versions of the components,
-#   do not fill the FILES array following the format: <folder>/<rpm>, no
-#   need to include the base URL.
-# -----------------------------------------------------------------------------
+deb_urls=$(bash get-cosmic-deb-urls.sh)
 
-echo "Obtaining copr2url..."
-wget https://github.com/mirkobrombin/copr2url/releases/download/continuous/copr2url -O /tmp/copr2url
-chmod +x /tmp/copr2url
+mkdir -p deb_files
+cd deb_files || exit
 
-echo "Generating list of RPM files..."
-FILES=($(/tmp/copr2url /usr/share/cosmic-repos.ini))
-
-echo "Downloading RPM files..."
-for file in "${FILES[@]}"; do
-  wget "${file}" -P ./rpm_files
+for url in $deb_urls; do
+  wget "$url"
 done
 
-echo "Installing required tools and dependencies..."
-sudo apt-get install -y rpm2cpio alien cpio libdisplay-info-bin libseat1 libxkbcommon0 libinput-bin libinput10 dbus xwayland
-
-echo "Converting and installing RPM files..."
-cd ./rpm_files || exit
-for rpm in *.rpm; do
-  rpm2cpio "$rpm" | cpio -idmv
-  sudo alien --scripts --to-deb "$rpm"
-done
-
-sudo dpkg -i *.deb
-sudo apt-get install -f -y
-
-echo "Installation complete!"
+sudo dpkg -i ./*.deb
+sudo apt-get -f install -y
