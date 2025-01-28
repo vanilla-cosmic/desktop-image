ea96eed2b891042e22488fb0c381729c114fc273
mirkobrombin <brombin@mirko.pm>
2025-01-28 15:55:16 +0100

    feat: use in-house built packages for cosmic components

--- includes.container/usr/share/get-cosmic-wip.sh
+++ includes.container/usr/share/get-cosmic-wip.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-deb_urls=$(bash /usr/share/get-cosmic-deb-urls.sh)
+export DEBIAN_FRONTEND=noninteractive
 
+echo "Downloading cosmic packages..."
+deb_urls=$(bash /usr/share/get-cosmic-deb-urls.sh)
 mkdir -p deb_files
 cd deb_files || exit
-
 for url in $deb_urls; do
-  wget "$url"
+  echo "Downloading $url"
+  wget -q --no-verbose "$url"
 done
 
-sudo dpkg -i ./*.deb
-sudo apt-get -f install -y
+echo "Installing required tools and dependencies..."
+sudo apt-get install -y greetd libdisplay-info-bin libdisplay-info2 libseat1 libxkbcommon0 libinput-bin libinput10 dbus xwayland geoclue-2.0 playerctl
+wget https://archive.neon.kde.org/unstable/pool/main/libd/libdisplay-info/libdisplay-info1_0.1.1-1+22.04+jammy+unstable+build3_amd64.deb
+sudo apt-get install -y ./libdisplay-info1_0.1.1-1+22.04+jammy+unstable+build3_amd64.deb
+
+echo "Installing cosmic packages"
+sudo apt-get install -y ./*.deb
+if [ $? -ne 0 ]; then
+  echo "Failed to install cosmic packages"
+  exit 1
+fi
