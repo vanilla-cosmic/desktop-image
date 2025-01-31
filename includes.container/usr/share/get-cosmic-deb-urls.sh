086545b94ccc7fea118ede9eda91d09ab6e3edcc
mirkobrombin <brombin@mirko.pm>
2025-01-31 10:33:17 +0100

    feat(ci): new brand

--- includes.container/usr/share/get-cosmic-deb-urls.sh
+++ includes.container/usr/share/get-cosmic-deb-urls.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-API_URL="https://api.github.com/repos/vanilla-cosmic/cosmic-epoch/releases/tags/continuous"
+API_URL="https://api.github.com/repos/singularityos-lab/cosmic-epoch/releases/tags/continuous"
 
 curl -s "$API_URL" | jq -r '.assets[]
   | select(.name | endswith(".deb"))
